66309cfe1cffe9392f3b7b0794dc0a1a2efa0664
ntalgio <ntalgio>
2025-08-18 04:31:48 +0300

    feat: implement server setup with socket.io and update socket connection logic for production environment

--- src/socket.js
+++ src/socket.js
@@ -1,4 +1,4 @@
 import { io } from 'socket.io-client';
 
-const socket = io(process.env.REACT_APP_SERVER_URL || 'http://192.168.1.85:4000');
+const socket = io(process.env.NODE_ENV === 'production' ? window.location.origin : (process.env.REACT_APP_SERVER_URL || 'http://192.168.1.85:4000'));
 export default socket;
