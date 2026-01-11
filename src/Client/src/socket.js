39b0182c657f45ab3c5a107809f162b7a239414c
GeorgiosNtallas <ntallasdev@gmail.com>
2026-01-12 01:37:32 +0200

    Remove fallback URL from socket connection configuration

--- src/Client/src/socket.js
+++ src/Client/src/socket.js
@@ -8,7 +8,7 @@ import { io } from "socket.io-client";
 //   },
 // });
 
-const socket = io(process.env.REACT_APP_SERVER_URL || "http://localhost:4000", {
+const socket = io(process.env.REACT_APP_SERVER_URL, {
   transports: ["websocket"], // helps avoid CORS issues
   auth: {
     roomSessionKey: sessionStorage.getItem("roomSessionKey"),
