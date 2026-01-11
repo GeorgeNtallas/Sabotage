19e33d0331debe230a173f2d495e098bb7eee20b
GeorgiosNtallas <ntallasdev@gmail.com>
2026-01-12 00:35:30 +0200

    folder

--- src/Server/db.js
+++ src/Server/db.js
@@ -1,9 +1,9 @@
-const mongoose = require('mongoose');
-const Room = require('./models/Room');
+const mongoose = require("mongoose");
+const Room = require("./Models/Room");
 
 async function connectDB() {
   if (!process.env.MONGODB_URI) {
-    throw new Error('MONGODB_URI not set');
+    throw new Error("MONGODB_URI not set");
   }
   // Mongoose >=6 uses sensible defaults; do not pass deprecated options.
   return mongoose.connect(process.env.MONGODB_URI);
